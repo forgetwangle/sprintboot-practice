907fcabee6e703f4ff726db637e119dee741a420
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-11-14 15:17:13 +0800

    Create 1.java

---
+++ 241029/014/1.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZypjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZypjApplication.class, args);
+	}
+
+}
+/**
+ai语句实例：
+	详细介绍@Test注解在Springboot中的使用
+*/
